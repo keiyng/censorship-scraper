d8b1aeda24f0802d092390406579869525039cf4
Kei <keiyin.ng5@gmail.com>
2019-02-22 15:09:49 -0500

    use logging

--- web_scraping.py
+++ web_scraping.py
@@ -1,19 +1,20 @@
 # encoding: utf-8
+import logging
+import argparse
+import threading
+from queue import Queue
 import time
 import sys
 import re
 import os
-import threading
 import urllib
-import argparse
-from queue import Queue
 from connections import mysql_database
-from variables import element_selector, local_path
+from constants import element_selector, local_path
 from selenium import webdriver
 
 EMOJI = re.compile('[\U00010000-\U0010ffff]', flags=re.UNICODE)
 
-def get_page(thread_name, search_term, queue):
+def get_page(search_term, queue):
 
    get_success = False
    get_failure = 0
@@ -22,44 +23,45 @@ def get_page(thread_name, search_term, queue):
        search_term = search_term.encode('utf-8')
    except UnicodeError:
        sys.exit('cannot encode search term to utf-8.')
+
    quoted_search_term = urllib.parse.quote(search_term)
 
    while get_success is False and get_failure < 3:
 
        try:
-           print('starting the driver for {}...'.format(thread_name))
+           logging.info('\nstarting the driver')
            driver = webdriver.PhantomJS(executable_path=local_path.phantomjs_path)
            ## necessary for elements to be located
            driver.set_window_size(1124, 850)
-           print('getting the page for {}...'.format(thread_name))
+           logging.info('\ngetting the page')
            driver.get('http://s.weibo.com/weibo/{}&Refer=STopic_box'.format(quoted_search_term))
            get_success = True
        except Exception as e:
-           print(str(e))
-           print('An error occured trying to visit the page for {}.'.format(thread_name))
+           logging.info('\nAn error occured trying to visit the page')
            get_success = False
            get_failure += 1
            time.sleep(5)
        else:
-          get_success, get_failure = expand_content(thread_name, driver, get_success, get_failure)
+          get_success, get_failure = expand_content(driver, get_success, get_failure)
 
    if get_success is True:
-       data = scrape(thread_name, driver)
+       data = scrape(driver)
        queue.put(data)
-       print('scraping done for {}.'.format(thread_name))
+       logging.info('\nscraping done')
 
    return
 
 
-def expand_content(thread_name, driver, get_success, get_failure):
+def expand_content(driver, get_success, get_failure):
 
    expand_success = False
    expand_failure = 0
+   
    while expand_success is False and expand_failure < 2:
        try:
            expand = driver.find_elements_by_partial_link_text(element_selector.EXPAND_TEXT_SELECTOR)
            clicked = 0
-           print('expanding the elements for {}...'.format(thread_name))
+           logging.info('\nexpanding elements')
            for ele in expand:
                ele.click()
                ## allow time for the click to complete
@@ -67,8 +69,7 @@ def expand_content(thread_name, driver, get_success, get_failure):
                clicked += 1
 
        except Exception as e:
-           print('An error occured trying to click the expand element for {}.'.format(thread_name))
-           print(str(e))
+           logging.info('\nAn error occured trying to expand element')
            expand_success = False
            expand_failure += 1
            time.sleep(5)
@@ -80,7 +81,7 @@ def expand_content(thread_name, driver, get_success, get_failure):
                driver.quit()
                get_success = False
                get_failure += 1
-               print('no. of expand and collapse do not match for {}. start again...'.format(thread_name))
+               logging.info('\nno. of expand and collapse do not match. Start again...')
                time.sleep(10)
                break
            else:
@@ -89,7 +90,7 @@ def expand_content(thread_name, driver, get_success, get_failure):
    return get_success, get_failure
 
 
-def scrape(thread_name, driver):
+def scrape(driver):
 
    full_content_div = driver.find_elements_by_css_selector(element_selector.FULL_CONTENT_SELECTOR)
 
@@ -115,12 +116,12 @@ def scrape(thread_name, driver):
    url = [ele.get_attribute("href") for ele in url]
 
    if len(url) != len(content):
-       print('url len:' + str(len(url)))
-       print('content len:' + str(len(content)))
+       logging.info('\nurl len:' + str(len(url)))
+       logging.info('\ncontent len:' + str(len(content)))
        driver.quit()
-       sys.exit('scrapped content not aligning for {}'.format(thread_name))
+       sys.exit('\nscraped content not aligning')
 
-   print('no. of posts scrapped for {}: {}'.format(thread_name, str(len(content))))
+   logging.info('\nno. of posts scraped: {}'.format(str(len(content))))
    simplified_url = [u[0:u.find('?')] for u in url]
    uid = [u.split('/')[-2] for u in simplified_url]
    pid = [u.split('/')[-1] for u in simplified_url]
@@ -161,22 +162,21 @@ def save_to_db(table, queue):
                    cur.execute('''INSERT INTO {} (content, url, uid, pid, mid, pubdate, tested, testdate, status)
                                    VALUES (%s, %s, %s, %s, %s, CURDATE(), DEFAULT, DEFAULT, DEFAULT)'''.format(table), \
                                    (data["content"][i], data["url"][i], data["uid"][i], data["pid"][i], data["mid"][i]))
-                   print('saved pid: {}'.format(data["pid"][i]))
+                   logging.info('\nsaved pid: {}'.format(data["pid"][i]))
 
                    saved += 1
                    cur.connection.commit()
                except Exception as e:
-                   print(str(e))
-                   print('unable to insert pid {} ... into table.'.format(data["pid"][i]))
+                   logging.info('\nunable to insert pid {} ... into table.'.format(data["pid"][i]))
                    continue
            else:
                skipped += 1
 
-   print('saved: {}; skipped: {}'.format(saved, skipped))
+   logging.info('\nsaved: {}; skipped: {}'.format(saved, skipped))
 
    cur.execute('SELECT COUNT(*) FROM {}'.format(table))
    no_of_rows = str(cur.fetchone()[0])
-   print('no. of rows in database: {}'.format(no_of_rows))
+   logging.info('\nno. of rows in database: {}'.format(no_of_rows))
 
    mysql_database.disconnect(conn, cur)
 
@@ -185,6 +185,8 @@ def save_to_db(table, queue):
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(threadName)s:%(message)s')
+
     parser = argparse.ArgumentParser()
     parser.add_argument('terms', nargs='+')
     parser.add_argument('table', nargs=1)
@@ -194,25 +196,22 @@ if __name__ == '__main__':
     table = args["table"][0]
 
     queue = Queue()
-    threads = []
 
     try:
-        for i in range(len(terms)):
-            thread = threading.Thread(target=get_page, args=('thread {}'.format(str(i)), terms[i], queue))
-            threads.append(thread)
+        threads = [threading.Thread(target=get_page, args=(terms[i], queue)) for i in range(len(terms))]
     except Exception as e:
-        print(str(e))
-        print('unable to start scraping threads.')
+        logging.info('\nunable to start scraping threads.')
         driver.quit()
 
     for thread in threads:
         thread.start()
+
+    # wait for all threads to complete.
     for thread in threads:
         thread.join()
 
     try:
         threading.Thread(target=save_to_db, args=(table, queue)).start()
     except Exception as e:
-        print(str(e))
-        print('unable to start database thread.')
+        logging.info('\nunable to start database thread.')
         driver.quit()
