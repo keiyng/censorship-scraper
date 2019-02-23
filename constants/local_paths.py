a6131a19395a0e6353b2554b7918ab9c71f0ff3e
Kei <keiyin.ng5@gmail.com>
2019-02-23 16:37:24 -0500

    updated .gitignore

--- constants/local_paths.py
+++
@@ -1,3 +0,0 @@
-phantomjs_path = '/Users/Kei/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs'
-python_path = '/usr/local/bin/python3'
-scraping_script_path = '/Users/Kei/Desktop/web_scraping/web_scraping.py'
