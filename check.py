caf58dea8c63b39c142a7088e4c6f2bcd7f8f9d1
Kei <keiyin.ng5@gmail.com>
2019-02-23 16:26:25 -0500

    updated availability checking logic

---
+++ check.py
@@ -0,0 +1,127 @@
+import sys
+import time
+import logging
+import weibo_api
+import requests as r
+from connections import mysql_database, phantomjs
+
+def select(num, table):
+    conn, cur = mysql_database.connect()
+    ## 1st test after 2 days
+    if num == '1':
+        cur.execute(
+        '''SELECT url, pid, mid from {}
+        WHERE pubdate < CURDATE() - INTERVAL 2 DAY
+        AND tested = "not yet" '''.format(table))
+    ## 2nd test after 14 days
+    elif num == '2':
+        cur.execute(
+        '''SELECT url, pid, mid from {}
+        WHERE retested = "not yet" 
+        AND pubdate < CURDATE() - INTERVAL 14 DAY
+        '''.format(table))
+    else:
+        sys.exit('unable to select appropriate data to test')
+
+    data = list(cur.fetchall())
+    logging.info('\nno. of posts to be tested: {}'.format(len(data)))
+
+    mysql_database.disconnect(conn, cur)
+
+    return data
+
+def get_page():
+    if len(data) > 0:
+        conn, cur = mysql_database.connect()
+        driver = phantomjs.start()
+        counter = 0
+
+        for url, pid, mid in data:
+            try:
+                driver.get(url)
+                counter += 1
+                print('{} visiting {}'.format(counter, url))
+                ## allow time for page to load (or possibly redirect)
+                time.sleep(20)
+            except Exception as e:
+                logging.info('An error occured trying to visit {}. \n{}'.format(url, e))
+                continue
+            else:
+                current_url = driver.current_url
+                update_db(driver, url, pid, mid, current_url, conn, cur, num, table)
+
+        mysql_database.disconnect(conn, cur)
+        driver.quit()
+
+    return
+
+def update_db(driver, url, pid, mid, current_url, conn, cur, num, table):
+    try:
+        ## if not available
+        if pid not in current_url:
+            logging.info('\npid {} not found in current url: {}'.format(pid, current_url))
+            logging.info('\noriginal url is: {}'.format(url))
+            if num == '1':             
+                cur.execute('''UPDATE {}
+                SET tested = "yes", testdate = CURDATE(), status = "not available"
+                WHERE mid = %s'''.format(table), \
+                (mid))
+            elif num == '2':
+                request = r.get('{}id={}&access_token={}'.format(weibo_api.BASE_STATUS_REQUEST_URL, mid, weibo_api.ACCESS_TOKEN_1)) 
+                response = request.json()
+                error = response['error']
+                print(error)
+                if 'limit' in error:
+                    sys.exit()
+                if 'Permission' in error:
+                    driver.get(url[:-9])
+                    print('visiting user page {}'.format(url[:-9]))
+                    time.sleep(20)
+                    user_url = driver.current_url
+                    print(user_url)
+                    if user_url.endswith('.com/us'):
+                        print("********* protected user **************")
+                        cur.execute('''UPDATE {}
+                        SET retested = "yes", retestdate = CURDATE(), status_2 = "protected" WHERE mid = %s'''.format(table), \
+                        (mid))
+                    else:
+                        print("system deleted")
+                        cur.execute('''UPDATE {}
+                        SET retested = "yes", retestdate = CURDATE(), status_2 = "not available", error = %s WHERE mid = %s'''.format(table), \
+                        (error, mid))
+                else:
+                    cur.execute('''UPDATE {}
+                    SET retested = "yes", retestdate = CURDATE(), status_2 = "not available", error = %s WHERE mid = %s'''.format(table), \
+                    (error, mid))
+        ## if available
+        else:
+            if num == '1':
+                cur.execute('''UPDATE {}
+                SET tested = "yes", testdate = CURDATE(), status = "available"
+                WHERE mid = %s'''.format(table), \
+                (mid))
+            elif num == '2':
+                print('{} is accessible;\ncurrent url is {}'.format(pid, current_url))
+                cur.execute('''UPDATE {}
+                SET retested = "yes", retestdate = CURDATE(), status_2 = "available"
+                WHERE mid = %s'''.format(table), \
+                (mid))
+
+    except Exception as e:
+        print('unable to update {}. {}'.format(pid, e))
+
+    else:
+        cur.connection.commit()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(message)s')
+
+    num = sys.argv[1]
+    table = sys.argv[2]
+
+    if num != '1' and num != '2':
+        sys.exit('invalid argument for check')
+    
+    data = select(num, table)
+    get_page()
